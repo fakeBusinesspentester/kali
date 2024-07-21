9d3b5814cd16bc189abdd441995e826fb506f4f2
fakeBusiness <geraldfernforder@mail.com>
2024-07-21 10:27:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ echo "[i] Updating and upgrading Kali (this will take a while)"
 apt-get update
 apt-get --yes --force-yes full-upgrade
 
+sudo apt install rkhunter
+sudo rkhunter -c
+sudo apt install chrootkit
+sudo chrootkit
+sudo apt install samhain
+
 echo "[i] Installing Xfce4 & xrdp (this will take a while as well)"
 
 apt-get --yes --force-yes install kali-desktop-xfce xorg xrdp
